b576ff4bd0a52bf12ee1ab20b40f50d24a579e8f
Salman Khan <salmantechhub336@gmail.com>
2026-05-16 16:18:52 +0500

    chit api and display data on the screen

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,24 @@ const App = () => {
 
   const getData = async() => {
     // console.log("data agaya hai");
-    const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=30');
-    // console.log(response.data);
-    setuserData(response.data)
+    const response = await axios.get('https://picsum.photos/v2/list?page=3&limit=100');
+    console.log(response.data);
+    setuserData(response.data);
     
   };
   let printUserData = 'No user Data available';
   if(userData.length>0){
     printUserData = userData.map(function(e , index){
       return <div>
-        <img src={e.download_url} alt="" />
+        <div className="h-40 w-44 rounded-xl overflow-hidden">
+        <img className="h-full w-full object-cover" src={e.download_url} alt="" />
       </div>
-
+      <h2>{e.author}</h2>
+      </div>
     })
   }
   return (
-    <div className="bg-black h-screen text-white p-4">
+    <div className="bg-black h-screen overflow-auto text-white p-4">
       <button
         onClick={() => {
           getData();
@@ -31,7 +33,7 @@ const App = () => {
       >
         Get Data
       </button>
-      <div>
+      <div className="flex flex-wrap gap-4">
         {printUserData}
       </div>
     </div>
